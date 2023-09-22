142f1bc7ccd31d8541711bf74cebde2d4f1208b6
AgusRiela <102192161+AgusRiela@users.noreply.github.com>
2023-09-22 16:25:02 -0300

    Entrega nº1 cv Agustina Riela

---
+++ html/informacion.js
@@ -0,0 +1,13 @@
+function funcionremis() { 
+    document.getElementById("informacion remis").innerHTML = " Año 2009 hasta 2019.Referencia:Lucio Esteberena 2494693463";
+  }
+  function funcionimprenta() { 
+    document.getElementById("informacion imprenta").innerHTML = "Año 2015 Referencia:Mariano Cestona 2494506530";
+  }
+  function funcionkiosco() { 
+    document.getElementById("informacion kiosco").innerHTML = "Año:2020 hasta 2022 Referencia: German Ocaño 2494660275";
+  }
+  function funciontecnosoft() { 
+    document.getElementById("informacion tecnosoft").innerHTML = "Año 2022 hasta 2023 Referencia:Matias Piscicelli 2494080726";
+  }
+  
